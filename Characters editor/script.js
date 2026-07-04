5d7045741686150b6519aaf5004e15bee578f549
Schwepsik <kingstonsc@mail.ru>
2026-07-05 00:41:50 +0300

    Add random stats

--- Characters editor/script.js
+++ Characters editor/script.js
@@ -13,22 +13,58 @@ const characters = [];
 
 const CHARACTER_CLASSES = {
    Warrior: {
-      hp: 1000,
-      attack: 175,
-      defense: 150,
-      mana: 80
+      hp:{
+         max:1150,
+         min: 850
+      },
+      attack: {
+         max:200,
+         min: 150
+      },
+      defense: {
+         max:175,
+         min: 125
+      },
+      mana: {
+         max:100,
+         min: 60
+      }
    },
    Mage: {
-      hp: 700,
-      attack: 275,
-      defense: 70,
-      mana: 100
+      hp: {
+         max: 900,
+         min: 500
+      },
+      attack: {
+         max: 325,
+         min: 275
+      },
+      defense: {
+         max: 90,
+         min: 50
+      },
+      mana: {
+         max: 125,
+         min: 75
+      }
    },
    Archer: {
-      hp: 1500,
-      attack: 60,
-      defense: 230,
-      mana: 40
+      hp: {
+         max: 1800,
+         min: 1200
+      },
+      attack: {
+         max: 70,
+         min: 50
+      },
+      defense: {
+         max: 250,
+         min: 200
+      },
+      mana: {
+         max: 50,
+         min: 30
+      }
    }
 };
 
@@ -36,17 +72,44 @@ class Character {
    constructor(name, characterClass) {
       this.name = name;
       this.characterClass = characterClass;
-      this.createStats();
    }
-   createStats() {
-      Object.assign(this, CHARACTER_CLASSES[this.characterClass]);
-   }
+}
+
+function createStats(character, characterClass) {
+   character.hp =
+      CHARACTER_CLASSES[characterClass].hp.min +
+      Math.floor(
+         Math.random() *
+         (CHARACTER_CLASSES[characterClass].hp.max - CHARACTER_CLASSES[characterClass].hp.min + 1)
+      );
+
+   character.attack =
+      CHARACTER_CLASSES[characterClass].attack.min +
+      Math.floor(
+         Math.random() *
+         (CHARACTER_CLASSES[characterClass].attack.max - CHARACTER_CLASSES[characterClass].attack.min + 1)
+      );
+
+   character.defense =
+      CHARACTER_CLASSES[characterClass].defense.min +
+      Math.floor(
+         Math.random() *
+         (CHARACTER_CLASSES[characterClass].defense.max - CHARACTER_CLASSES[characterClass].defense.min + 1)
+      );
+
+   character.mana =
+      CHARACTER_CLASSES[characterClass].mana.min +
+      Math.floor(
+         Math.random() *
+         (CHARACTER_CLASSES[characterClass].mana.max - CHARACTER_CLASSES[characterClass].mana.min + 1)
+      );
 }
 
 btn.addEventListener("click", () => {
    const name = nameInput.value;
    const characterClass = classSelect.value;
    const character = new Character(name, characterClass);
+   createStats(character, characterClass);
    characters.push(character);
    renderCharacters();
 });
